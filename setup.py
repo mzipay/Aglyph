e1ec596edfde099652bf562476f778f8df26f813
Matthew Zipay <mattz@ninthtest.info>
2018-03-15 22:51:06 -0400

    removed markdown from setup.py description (not supported by PyPI)

--- setup.py
+++ setup.py
@@ -8,31 +8,16 @@ setup(
     description=
         "Aglyph is a Dependency Injection framework for Python.",
     long_description="""\
-* supports type 2 (setter) and type 3 (constructor) dependency injection
-* can assemble *prototype*, *singleton*, *borg*, and *weakref* components
-* supports templates (i.e. component inheritance) and lifecycle methods
-* works with any kind of object creation pattern you'll encounter:
-  * constructor
-  * factory function or method
-  * object creation hidden behind attribute or property access
-* is configured declaratively, either programmatically through a fluent API or
-  using a simple XML syntax (see the `Aglyph DTD
-  <https://github.com/mzipay/Aglyph/blob/master/resources/aglyph-context.dtd>`_)
-* is non-intrusive:
-  * only one dependency (`Autologging
-    <http://ninthtest.info/python-autologging/>`_) beyond the Python standard
-    library
-  * does not require modification of existing source code (i.e. no
-    decorators, specific naming conventions, or any other kind of
-    syntactic "magic" necessary)
-* can inject not only 3rd-party dependencies, but also **dependents**
-* runs on Python 2.7 and 3.4+ using the same codebase
-* is proactively tested on `CPython <https://www.python.org/>`_,
-  `Jython <http://www.jython.org/>`_, `IronPython <http://ironpython.net/>`_,
-  `PyPy <http://pypy.org/>`_, and
-  `Stackless Python <https://bitbucket.org/stackless-dev/stackless/wiki/Home>`_
-* is fully logged *and traced* for ease of troubleshooting (note: tracing is
-  disabled by default, and can be activated by setting an environment variable)
++ supports type 2 (setter) and type 3 (constructor) dependency injection
++ can assemble prototype, singleton, borg, and weakref components
++ supports templates (i.e. component inheritance) and lifecycle methods
++ works with any kind of object creation pattern you'll encounter (constructor, factory function/method, attribute/property access, import)
++ configured declaratively, either programmatically through a fluent API or using a simple XML syntax
++ non-intrusive wiring style does not require modification of any existing sources (no decorators, no naming conventions, no syntactic "magic" needed)
++ can inject not only 3rd-party dependencies, but also dependents (even Java/.NET classes under Jython/IronPython)
++ runs on Python 2.7 and 3.4+ using the same codebase
++ proactively tested on CPython, Jython, IronPython, PyPy and Stackless Python
++ fully logged and traced for easy troubleshooting (note: tracing is disabled by default, and can be activated by setting an environment variable)
 """,
     author="Matthew Zipay",
     author_email="mattz@ninthtest.info",
